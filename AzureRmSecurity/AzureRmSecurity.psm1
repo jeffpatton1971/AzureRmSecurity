f6a5e401ef69e05171132ba94376b6aad49622ac
Jeff Patton <jeffrey@patton-tech.com>
2017-03-29 09:55:50 -0500

    There are no types for tasks

--- AzureRmSecurity/AzureRmSecurity.psm1
+++ AzureRmSecurity/AzureRmSecurity.psm1
@@ -148,13 +148,6 @@ function Get-AzureRmSecurityTask
 				Type = [object]
 				Position = 0
 				Mandatory = $true
-			},
-			@{
-				Name = 'ResourceType'
-				Type = [string]
-				Position = 2
-				Manndatory = $false
-				ValidateSet = (Get-AzureRmResource |Select-Object -Property ResourceType -Unique |Select-Object -ExpandProperty ResourceType)
 			}
 		)
 		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
@@ -163,7 +156,6 @@ function Get-AzureRmSecurityTask
 	begin
 	{
 		$Subscription = $PSBoundParameters['Subscription'];
-		$ResourceType = $PSBoundParameters['ResourceType'];
 	}
 
 	process
@@ -176,14 +168,7 @@ function Get-AzureRmSecurityTask
 			$ResourceId = "/subscriptions/$($Subscription.SubscriptionId)/providers/microsoft.Security/tasks";
 			$ApiVersion = '2015-06-01-preview';
 
-			if ($ResourceType)
-			{
-				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion |Where-Object -Property ResourceType -EQ $ResourceType;
-			}
-			else
-			{
-				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion;
-			}
+			Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion;
 		}
 		catch
 		{
