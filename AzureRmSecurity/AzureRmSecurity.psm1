1273d3334b58a4815667c8176c6d1ef093a93a71
Jeff Patton <jeffrey@patton-tech.com>
2017-03-29 09:49:54 -0500

    Added get-azurermsecuritytask

--- AzureRmSecurity/AzureRmSecurity.psm1
+++ AzureRmSecurity/AzureRmSecurity.psm1
@@ -129,3 +129,69 @@ function Get-AzureRmSecurityAlert
 	{
 	}
 }
+
+function Get-AzureRmSecurityTask
+{
+	<#
+	https://msdn.microsoft.com/en-us/library/mt704053.aspx
+	#>
+	[CmdletBinding()]
+	param
+	(
+	)
+
+	DynamicParam
+	{
+		$DynamicParameters = @(
+			@{
+				Name = 'Subscription'
+				Type = [object]
+				Position = 0
+				Mandatory = $true
+			},
+			@{
+				Name = 'ResourceType'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+				ValidateSet = (Get-AzureRmResource |Select-Object -Property ResourceType -Unique |Select-Object -ExpandProperty ResourceType)
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
+	}
+
+	begin
+	{
+		$Subscription = $PSBoundParameters['Subscription'];
+		$ResourceType = $PSBoundParameters['ResourceType'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			$ResourceId = "/subscriptions/$($Subscription.SubscriptionId)/providers/microsoft.Security/tasks";
+			$ApiVersion = '2015-06-01-preview';
+
+			if ($ResourceType)
+			{
+				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion |Where-Object -Property ResourceType -EQ $ResourceType;
+			}
+			else
+			{
+				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+
+	end
+	{
+	}
+}
