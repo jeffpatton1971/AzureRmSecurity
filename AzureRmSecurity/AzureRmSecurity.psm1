0fad13a3ab33bbae17ae9bd55d3429f28fe1d18d
Jeff Patton <jeffrey@patton-tech.com>
2017-03-29 09:30:07 -0500

    Updated function to use dynamic parameters and added utility module with the dynamic parameter code

--- AzureRmSecurity/AzureRmSecurity.psm1
+++ AzureRmSecurity/AzureRmSecurity.psm1
@@ -1,19 +1,62 @@
 function Get-AzureRmSecurity
 {
-	[object]$Subscription
+	[CmdletBinding()]
+	param
+	(
+	)
 
-	try
+	DynamicParam
 	{
-		$ErrorActionPreference = 'Stop';
-		$Error.Clear();
-
-		$ResourceId = "/subscriptions/$($Subscription.SubscriptionId)/providers/microsoft.Security/securityStatuses";
-		$ApiVersion = '2015-06-01-preview';
-
-		Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion;
+		$DynamicParameters = @(
+			@{
+				Name = 'Subscription'
+				Type = [object]
+				Position = 0
+				Mandatory = $true
+			},
+			@{
+				Name = 'ResourceType'
+				Type = [string]
+				Position = 2
+				Manndatory = $false
+				ValidateSet = (Get-AzureRmResource |Select-Object -Property ResourceType -Unique |Select-Object -ExpandProperty ResourceType)
+			}
+		)
+		$DynamicParameters |ForEach-Object {New-Object -TypeName psobject -Property $_} |New-DynamicParameter;
 	}
-	catch
+
+	begin
 	{
-		throw $_;
+		$Subscription = $PSBoundParameters['Subscription'];
+		$ResourceType = $PSBoundParameters['ResourceType'];
+	}
+
+	process
+	{
+		try
+		{
+			$ErrorActionPreference = 'Stop';
+			$Error.Clear();
+
+			$ResourceId = "/subscriptions/$($Subscription.SubscriptionId)/providers/microsoft.Security/securityStatuses";
+			$ApiVersion = '2015-06-01-preview';
+
+			if ($ResourceType)
+			{
+				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion |Where-Object -Property ResourceType -EQ $ResourceType;
+			}
+			else
+			{
+				Get-AzureRmResource -ResourceId $ResourceId -ApiVersion $ApiVersion;
+			}
+		}
+		catch
+		{
+			throw $_;
+		}
+	}
+
+	end
+	{
 	}
 }
